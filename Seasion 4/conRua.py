a2148a3333b3f979fece5dd578e8da2f1c2c5fcb
leducviet <ducviet221204@gmail.com>
2019-10-13 11:20:59 +0700

    abc

---
+++ Seasion 4/conRua.py
@@ -0,0 +1,21 @@
+from turtle import *
+shape("turtle")
+color("blue")
+speed()
+
+for i in range(5):
+    for g in range(4):
+        fd(300)
+        rt(90)
+    rt(72)
+
+
+
+
+
+
+
+
+
+
+mainloop()
